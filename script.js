636468ea1fc16715e7c276dac3fbac98c6a7ecdc
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-04-13 08:34:15 +1200

    // DOM and Event Fundamentals (DOM explanation)
    // > DOM (Document Object Model) 
    //  - Representation of HTML documents. Allowing Javascript to access the elements and styles and manipulate them
    //  - DOM is structured in a TREE Structure, each HTML element is an object
    //  - DOM is not a part of Javascript but WEB API (DOM methods and properties) that we can access from Javascript code

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // DOM and Event Fundamentals
-//- DOM Manipulation
-//-
-//-
+// - DOM Manipulation (Making Javascipth to interact with the web page )
+// -
+// -
 
 'use strict';
+// > DOM (Document Object Model) 
+//  - Representation of HTML documents. Allowing Javascript to access the elements and styles and manipulate them
+//  - DOM is structured in a TREE Structure, each HTML element is an object
+//  - DOM is not a part of Javascript but WEB API (DOM methods and properties) that we can access from Javascript code
 
-// Selecting an element and its text content (.textContent) from HTML using Javascript
+// > Selecting an element and its text content (.textContent) from HTML using Javascript
 console.log(document.querySelector('.message').textContent);

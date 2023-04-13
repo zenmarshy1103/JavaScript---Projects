2f623568969cf9e0a421cf1cf3380df5c52710f1
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-04-14 07:46:20 +1200

    Selecting and Manipulating Elements
    // - DOM Manipulation (Making Javascipt to interact with the web page )
    // - Setting the text content of an element (Any Element)
    // -

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// DOM and Event Fundamentals
-// - DOM Manipulation (Making Javascipth to interact with the web page )
-// -
+// Selecting and Manipulating Elements
+// - DOM Manipulation (Making Javascipt to interact with the web page )
+// - Setting the text content of an element (Any Element)
 // -
 
 'use strict';
@@ -11,3 +11,17 @@
 
 // > Selecting an element and its text content (.textContent) from HTML using Javascript
 console.log(document.querySelector('.message').textContent);
+
+// > Set the content for the element
+document.querySelector('.message').textContent = 'Correct Number Guessed!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+// > Set the content for an input element that needs a value inputted from the user
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+

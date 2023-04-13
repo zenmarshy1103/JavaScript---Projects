e8ef2f737bc6e4144ce4ce8e7b34c4cb80136e87
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-04-14 09:18:25 +1200

    Implementing the game Logic for the Guess My Number Project
    // - DOM Manipulation (Making Javascript to interact with the web page )
    // - 
    // -

--- script.js
+++ script.js
@@ -1,43 +1,58 @@
-// Handling Click Events
-// - DOM Manipulation (Making Javascipt to interact with the web page )
-// - Handling click events
+// Implementing the game Logic for the Guess My Number Project
+// - DOM Manipulation (Making Javascript to interact with the web page )
+// - 
 // -
 
 'use strict';
-// > DOM (Document Object Model) 
+// DOM (Document Object Model) 
 //  - Representation of HTML documents. Allowing Javascript to access the elements and styles and manipulate them
 //  - DOM is structured in a TREE Structure, each HTML element is an object
 //  - DOM is not a part of Javascript but WEB API (DOM methods and properties) that we can access from Javascript code
 
-// Selecting an element and its text content (.textContent) from HTML using Javascript
-// console.log(document.querySelector('.message').textContent);
+// > Define the secret number
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = secretNumber;
 
-// Set the content for the element
-// document.querySelector('.message').textContent = 'Correct Number Guessed!';
-// console.log(document.querySelector('.message').textContent);
+let score = 20;
 
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// Set the content for an input element that needs a value inputted from the user
-// console.log(document.querySelector('.guess').value);
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
-
-// > Listen for a click on the check button element with (.addEventListener(typeOfEvent) ) method
-// > and triggers a event handling function (.addEventLister(typeOfEvent, eventHandler))
+// > Guess Logic for the game
+// Listen for a click on the check button element with (.addEventListener(typeOfEvent) ) method
+// and triggers a event handling function (.addEventLister(typeOfEvent, eventHandler))
 document.querySelector('.check').addEventListener('click', function() {    //function is another value when returned so can be used as an input argument
     // console.log(document.querySelector('.guess').value);
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof(guess));
 
-    // Check if the guess field is entered
+    // > Check if the guess field is entered or value entered is too high or low or correct and changes the score
     if (!guess) {
         document.querySelector('.message').textContent = '!No Number was Entered!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number!';       
+    } else if (guess > secretNumber) {
 
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Guess is too high';
+            score--;  //decrease the score   
+            document.querySelector('.score').textContent = score;
+        }else {
+            document.querySelector('.message').textContent = 'You Lost !!!';
+            document.querySelector('.score').textContent = 0;
+        }
+    
+    } else if (guess < secretNumber) {
+        // > Repeating code - will deal with this later 
+        if(score > 1) {
+            document.querySelector('.message').textContent = 'Guess is too low';
+            score--;  //decrease the score   
+            document.querySelector('.score').textContent = score;
+        }else {
+            document.querySelector('.message').textContent = 'You Lost !!!';
+            document.querySelector('.score').textContent = 0;
+        }
+        
+       
     }
 
-    // document.querySelector('.message').textContent = 'Correct Number Guessed!'
 });  
 
 
